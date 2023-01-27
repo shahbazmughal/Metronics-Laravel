92cca55b73a5befdd9822c87e4cbc2bfdd1fc4b9
usman ahmad <usmanahmadhh@gmail.com>
2023-01-27 20:08:09 +0500

    laravel header menu set

--- resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
@@ -1,78 +1,49 @@
-<!--begin::Navbar-->
 <div class="app-navbar flex-shrink-0">
-    <!--begin::Search-->
-    <div class="app-navbar-item align-items-stretch ms-1 ms-lg-3">
-        @include(config('settings.KT_THEME_LAYOUT_DIR').'/partials/sidebar-layout/search/_dropdown')
-    </div>
-    <!--end::Search-->
-    <!--begin::Activities-->
     <div class="app-navbar-item ms-1 ms-lg-3">
-        <!--begin::Drawer toggle-->
         <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px" id="kt_activities_toggle">
-            <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
-            {!! getSvgIcon('duotune/general/gen032.svg', 'svg-icon svg-icon-1') !!}
-            <!--end::Svg Icon-->
+            @include('partials/menus/_processing-menu')
         </div>
-        <!--end::Drawer toggle-->
     </div>
-    <!--end::Activities-->
-    <!--begin::Notifications-->
     <div class="app-navbar-item ms-1 ms-lg-3">
-        <!--begin::Menu- wrapper-->
+        <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px"  data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+            {!! getSvgIcon('duotune/general/processes.svg', 'svg-icon svg-icon-1') !!}
+        </div>
+        @include('partials/menus/_processing-menu')
+    </div>
+    <div class="app-navbar-item ms-1 ms-lg-3">
         <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-            <!--begin::Svg Icon | path: icons/duotune/general/gen022.svg-->
-            {!! getSvgIcon('duotune/general/gen022.svg', 'svg-icon svg-icon-1') !!}
-            <!--end::Svg Icon-->
+            {!! getSvgIcon('duotune/general/issues.svg', 'svg-icon svg-icon-1') !!}
         </div>
-        @include('partials/menus/_notifications-menu')
-        <!--end::Menu wrapper-->
+        @include('partials/menus/_issues-menu')
     </div>
-    <!--end::Notifications-->
-    <!--begin::Chat-->
     <div class="app-navbar-item ms-1 ms-lg-3">
-        <!--begin::Menu wrapper-->
-        <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px position-relative" id="kt_drawer_chat_toggle">
-            <!--begin::Svg Icon | path: icons/duotune/communication/com012.svg-->
-            {!! getSvgIcon('duotune/communication/com012.svg', 'svg-icon svg-icon-1') !!}
-            <!--end::Svg Icon-->
+        <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px position-relative" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+            {!! getSvgIcon('duotune/general/gen022.svg', 'svg-icon svg-icon-1') !!}
             <span class="bullet bullet-dot bg-success h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
         </div>
-        <!--end::Menu wrapper-->
+        @include('partials/menus/_notifications-menu')
     </div>
-    <!--end::Chat-->
-    <!--begin::My apps links-->
     <div class="app-navbar-item ms-1 ms-lg-3">
-        <!--begin::Menu wrapper-->
         <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-            <!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
-            {!! getSvgIcon('duotune/general/gen025.svg', 'svg-icon svg-icon-1') !!}
-            <!--end::Svg Icon-->
+            {!! getSvgIcon('duotune/general/app.svg', 'svg-icon svg-icon-1') !!}
         </div>
         @include('partials/menus/_my-apps-menu')
-        <!--end::Menu wrapper-->
     </div>
-    <!--end::My apps links-->
-    <!--begin::Theme mode-->
+    <div class="app-navbar-item ms-1 ms-lg-3">
+        <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+            {!! getSvgIcon('duotune/general/gen025.svg', 'svg-icon svg-icon-1') !!}
+        </div>
+        @include('partials/menus/_quick-links-menu')
+    </div>
     <div class="app-navbar-item ms-1 ms-lg-3">
         @include('partials/theme-mode/_main')
     </div>
-    <!--end::Theme mode-->
-    <!--begin::User menu-->
+    <div class="app-navbar-item ms-1 ms-lg-3">
+    </div>
     <div class="app-navbar-item ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
-        <!--begin::Menu wrapper-->
         <div class="cursor-pointer symbol symbol-35px symbol-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
             <img src="{{ image('avatars') }}/{{ isset(Auth::user()->photo) ? Auth::user()->photo : 'blank.png'  }}" alt="user" />
         </div>
         @include('partials/menus/_user-account-menu')
-        <!--end::Menu wrapper-->
     </div>
-    <!--end::User menu-->
-    <!--begin::Header menu toggle-->
-    <!-- <div class="app-navbar-item d-lg-none ms-2 me-n3" title="Show header menu">
-        <div class="btn btn-icon btn-active-color-primary w-35px h-35px" id="kt_app_header_menu_toggle">
-            {!! getSvgIcon('duotune/text/txt001.svg', 'svg-icon svg-icon-1') !!}
-        </div>
-    </div> -->
-    <!--end::Header menu toggle-->
 </div>
-<!--end::Navbar-->
---
+++ resources/views/partials/menus/_issues-menu.blade.php
@@ -0,0 +1,45 @@
+<div class="menu menu-sub menu-sub-dropdown menu-column w-250px w-lg-325px quick-links" data-kt-menu="true" data-popper-placement="bottom-end">
+  <div class="d-flex flex-column flex-center bgi-no-repeat rounded-top px-9 py-10" style="background-image:url({{ image('misc/menu-header-bg.jpg')}})">
+    <h3 class="text-white fw-bold"> Issues <span class="fs-8 opacity-75 ms-3 badge badge-light-info">3</span></h3>
+  </div>
+  <div class="scroll-y mh-275px m-0 px-0">
+    <div class="d-flex flex-stack py-4 px-5">
+      <div class="d-flex align-items-start">
+        <div class="symbol symbol-35px me-4">
+          <span class="symbol-label bg-light-warning">
+            <span class="svg-icon svg-icon-2 svg-icon-primary">
+              <i class="fa fa-times text-warning fa-lg"></i>
+            </span>
+          </span>
+        </div>
+        <div class="d-flex flex-column">
+          <span class="fs-7 mb-1 text-gray-800 fw-semibold"> Expected response code 354 but got code "503", with message "503 RCPT command expected" </span>
+          <span class="fs-8 text-muted fw-semibold mb-1">Last Checked Jan 28, 2023 04:52:59 AM </span>
+          <button type="button" class="btn btn-success btn-sm resolve-btn"> Mark Resolved </button>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex flex-stack py-4 px-5">
+      <div class="d-flex align-items-start">
+        <div class="symbol symbol-35px me-4">
+          <span class="symbol-label bg-light-warning">
+            <span class="svg-icon svg-icon-2 svg-icon-primary">
+              <i class="fa fa-times text-warning fa-lg"></i>
+            </span>
+          </span>
+        </div>
+        <div class="d-flex flex-column">
+          <span class="fs-7 mb-1 text-gray-800 fw-semibold"> Expected response code 354 but got code "503", with message "503 RCPT command expected" </span>
+          <span class="fs-8 text-muted fw-semibold mb-1">Last Checked Jan 26, 2023 11:45:55 PM </span>
+          <button type="button" class="btn btn-success btn-sm resolve-btn"> Mark Resolved </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-3 text-center border-top">
+    <a href="#" class="btn btn-color-gray-600 btn-active-color-primary"> View All <span class="svg-icon svg-icon-5">
+        <i class="fa fa-arrow-right fs-8"></i>
+      </span>
+    </a>
+  </div>
+</div>
