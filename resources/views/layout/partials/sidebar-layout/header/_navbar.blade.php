21627e1217fa52c1e59eb5181c55a79ac6424a1e
usman ahmad <usmanahmadhh@gmail.com>
2023-01-31 15:51:11 +0500

    svg movie to resources folder

--- resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="app-navbar flex-shrink-0">
     <div class="app-navbar-item ms-1 ms-lg-3">
         <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px"  data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-            {!! getSvgIcon('duotune/general/setup.svg', 'svg-icon svg-icon-1') !!}
+            {!! getSvgIcon('general/setup.svg', 'svg-icon svg-icon-1') !!}
         </div>
         @include('partials/menus/_setup-menu')
     </div>
